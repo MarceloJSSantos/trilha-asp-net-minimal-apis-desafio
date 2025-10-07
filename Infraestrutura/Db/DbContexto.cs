f24402dbf3a1b0d605f9a88dd7b61482b1da1805
MarceloJSSantos <marcelojssantos2012@gmail.com>
2025-10-07 12:00:01 -0300

    Configuração BD MySQL * Configuração do Contexto do BD * Criação da tabela Administradores * Criação das migrations de Administradores

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoFinalMinimalAPI.Dominio.Entidades;
+
 namespace ProjetoFinalMinimalAPI.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
+    private readonly IConfiguration _configuracaoAppSetting;
 
+    public DbContexto(IConfiguration configuracaoAppSetting)
+    {
+        _configuracaoAppSetting = configuracaoAppSetting;
+    }
+
+    public DbSet<Administrador> Admnistradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSetting.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using ProjetoFinalMinimalAPI.Dominio.DTOs;
+using ProjetoFinalMinimalAPI.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+}
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
